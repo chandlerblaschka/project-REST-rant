838471abcfe8e430c5dbb8fec21553e97cb66732
chandlerblaschka <cblaschka@live.com>
2021-12-06 20:14:37 -0600

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>Home</h1>
+                <div className="homeImage">
+                    <img src="/images/salad-home-image.jpg" alt="Harvest Salad"/>
+                </div>
+                <div>
+                Photo by <a href="https://unsplash.com/@annapelzer?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anna Pelzer</a> on <a href="https://unsplash.com/s/photos/free-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,18 +3,26 @@ const Def = require('../default')
 
 function index(data) {
     let placesFormatted = data.places.map((place) => {
-        return(
-            <div>
+        return (
+            <div className="col-sm-6">
                 <h2>{place.name}</h2>
-                <img src={place.pic} alt={place.name}></img>
+                <p clasName="text-center">
+                    {place.cuisines}
+                </p>
+                <img src={place.pic} alt={place.name} />
+                <p clasName="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
-    return(
+    return (
         <Def>
             <main>
-                <h1>Places Index Page</h1>
-                {placesFormatted}
+                <h1>Places to Rant or Rave About</h1>
+                <div className="row">
+                    {placesFormatted}
+                </div>
             </main>
         </Def>
     )
