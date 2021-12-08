c2a13516561e2a0be4b4ad9f9bac6b14f1dbbf9e
chandlerblaschka <cblaschka@live.com>
2021-12-08 08:24:40 -0600

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,37 @@
 const router = require('express').Router()
+var bodyParser = require('body-parser')
 
-router.get('/', (req, res) => {
+// create application/json parser
+//var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+router.get("/new", (req, res) => {
+    res.render("places/New")
+})
+
+router.get("/", (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: '/images/h-thai-food.jpg'
-      }, {
+    }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-cafe.jpg'
-      }
-    ]
-    res.render('places/index', { places})
+        pic: 'images/cat-cafe.jpg'
+    }]
+    res.render("places/index", { places })
 })
 
-router.get('/new', (req, res) => {
-  res.render('places/new')
+router.post("/", urlencodedParser, (req, res) => {
+    const obj = JSON.parse(JSON.stringify(req.body))
+    console.log(obj);
+    res.send("POST /places")
 })
 
-
-module.exports = router;
+module.exports = router
